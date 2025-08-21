fa92a0f65a5ca29842f4c613a29491a663d7c5be
Tom Genlis <tom.genlis@gmail.com>
2025-08-21 13:54:02 +0200

    feat: yes

--- runner/generated_block.py
+++ runner/generated_block.py
@@ -7,34 +7,6 @@ CIN = 3
 H = 32
 W = 32
 
-class LayerNorm2d(nn.Module):
-    def __init__(self, c, eps=1e-6):
-        super().__init__()
-        self.ln = nn.LayerNorm(c, eps=eps)
-
-    def forward(self, x):
-        # apply LayerNorm over channels in channels-last order
-        x = x.permute(0, 2, 3, 1)
-        x = self.ln(x)
-        x = x.permute(0, 3, 1, 2)
-        return x
-
-class MHSA2D(nn.Module):
-    def __init__(self, c, heads=8, attn_drop=0.0, proj_drop=0.0):
-        super().__init__()
-        self.attn = nn.MultiheadAttention(embed_dim=c, num_heads=int(heads), dropout=float(attn_drop), batch_first=True)
-        self.proj = nn.Linear(c, c)
-        self.proj_drop = nn.Dropout(p=float(proj_drop))
-
-    def forward(self, x):
-        # x: (N,C,H,W) -> (N,HW,C)
-        N, C, H, W = x.shape
-        x_seq = x.permute(0, 2, 3, 1).reshape(N, H*W, C)
-        out, _ = self.attn(x_seq, x_seq, x_seq)
-        out = self.proj_drop(self.proj(out))
-        out = out.reshape(N, H, W, C).permute(0, 3, 1, 2)
-        return out
-
 class GeneratedBlock(nn.Module):
     def __init__(self, in_channels=3):
         super().__init__()
@@ -46,69 +18,79 @@ class GeneratedBlock(nn.Module):
 class GeneratedModel(nn.Module):
     def __init__(self, in_channels=3):
         super().__init__()
-        self.m_0 = nn.Conv2d(3, 384, kernel_size=16, stride=16, padding=0, bias=False)
-        self.m_1 = LayerNorm2d(384)
-        self.m_2 = MHSA2D(384, heads=8, attn_drop=0, proj_drop=0)
-        self.m_3 = nn.Identity()
-        self.m_4 = LayerNorm2d(384)
-        self.m_5 = nn.Conv2d(384, 384, kernel_size=1, stride=1, padding=0, bias=False)
-        self.m_6 = nn.GELU()
-        self.m_7 = nn.Dropout(p=0.5)
-        self.m_8 = nn.Conv2d(384, 384, kernel_size=1, stride=1, padding=0, bias=False)
-        self.m_9 = nn.Identity()
-        self.m_10 = nn.Identity()
-        self.m_11 = LayerNorm2d(384)
-        self.m_12 = MHSA2D(384, heads=8, attn_drop=0, proj_drop=0)
-        self.m_13 = nn.Identity()
-        self.m_14 = LayerNorm2d(384)
-        self.m_15 = nn.Conv2d(384, 384, kernel_size=1, stride=1, padding=0, bias=False)
-        self.m_16 = nn.GELU()
-        self.m_17 = nn.Dropout(p=0.5)
-        self.m_18 = nn.Conv2d(384, 384, kernel_size=1, stride=1, padding=0, bias=False)
-        self.m_19 = nn.Identity()
-        self.m_20 = nn.Identity()
-        self.m_21 = LayerNorm2d(384)
-        self.m_22 = MHSA2D(384, heads=8, attn_drop=0, proj_drop=0)
-        self.m_23 = nn.Identity()
-        self.m_24 = LayerNorm2d(384)
-        self.m_25 = nn.Conv2d(384, 384, kernel_size=1, stride=1, padding=0, bias=False)
-        self.m_26 = nn.GELU()
-        self.m_27 = nn.Dropout(p=0.5)
-        self.m_28 = nn.Conv2d(384, 384, kernel_size=1, stride=1, padding=0, bias=False)
+        self.m_0 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, groups=1, dilation=1, bias=False)
+        self.m_1 = nn.BatchNorm2d(64)
+        self.m_2 = nn.ReLU(inplace=True)
+        self.m_3 = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1, groups=1, dilation=1, bias=False)
+        self.m_4 = nn.BatchNorm2d(64)
+        self.m_5 = nn.ReLU(inplace=True)
+        self.m_6 = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1, groups=1, dilation=1, bias=False)
+        self.m_7 = nn.BatchNorm2d(64)
+        self.m_8 = nn.Identity()
+        self.m_9 = nn.ReLU(inplace=True)
+        self.m_10 = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1, groups=1, dilation=1, bias=False)
+        self.m_11 = nn.BatchNorm2d(64)
+        self.m_12 = nn.ReLU(inplace=True)
+        self.m_13 = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1, groups=1, dilation=1, bias=False)
+        self.m_14 = nn.BatchNorm2d(64)
+        self.m_15 = nn.Identity()
+        self.m_16 = nn.ReLU(inplace=True)
+        self.m_17 = nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1, groups=1, dilation=1, bias=False)
+        self.m_18 = nn.BatchNorm2d(128)
+        self.m_19 = nn.ReLU(inplace=True)
+        self.m_20 = nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1, groups=1, dilation=1, bias=False)
+        self.m_21 = nn.BatchNorm2d(128)
+        self.m_22 = nn.Identity()
+        self.m_proj_22 = nn.Sequential(
+            nn.Conv2d(64, 128, kernel_size=1, stride=2, padding=0, bias=False),
+            nn.BatchNorm2d(128)
+        )
+        self.m_23 = nn.ReLU(inplace=True)
+        self.m_24 = nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1, groups=1, dilation=1, bias=False)
+        self.m_25 = nn.BatchNorm2d(128)
+        self.m_26 = nn.ReLU(inplace=True)
+        self.m_27 = nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1, groups=1, dilation=1, bias=False)
+        self.m_28 = nn.BatchNorm2d(128)
         self.m_29 = nn.Identity()
-        self.m_30 = nn.Identity()
-        self.m_31 = LayerNorm2d(384)
-        self.m_32 = MHSA2D(384, heads=8, attn_drop=0, proj_drop=0)
-        self.m_33 = nn.Identity()
-        self.m_34 = LayerNorm2d(384)
-        self.m_35 = nn.Conv2d(384, 384, kernel_size=1, stride=1, padding=0, bias=False)
-        self.m_36 = nn.GELU()
-        self.m_37 = nn.Dropout(p=0.5)
-        self.m_38 = nn.Conv2d(384, 384, kernel_size=1, stride=1, padding=0, bias=False)
-        self.m_39 = nn.Identity()
-        self.m_40 = nn.Identity()
-        self.m_41 = LayerNorm2d(384)
-        self.m_42 = MHSA2D(384, heads=8, attn_drop=0, proj_drop=0)
+        self.m_30 = nn.ReLU(inplace=True)
+        self.m_31 = nn.Conv2d(128, 256, kernel_size=3, stride=2, padding=1, groups=1, dilation=1, bias=False)
+        self.m_32 = nn.BatchNorm2d(256)
+        self.m_33 = nn.ReLU(inplace=True)
+        self.m_34 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1, groups=1, dilation=1, bias=False)
+        self.m_35 = nn.BatchNorm2d(256)
+        self.m_36 = nn.Identity()
+        self.m_proj_36 = nn.Sequential(
+            nn.Conv2d(128, 256, kernel_size=1, stride=2, padding=0, bias=False),
+            nn.BatchNorm2d(256)
+        )
+        self.m_37 = nn.ReLU(inplace=True)
+        self.m_38 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1, groups=1, dilation=1, bias=False)
+        self.m_39 = nn.BatchNorm2d(256)
+        self.m_40 = nn.ReLU(inplace=True)
+        self.m_41 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1, groups=1, dilation=1, bias=False)
+        self.m_42 = nn.BatchNorm2d(256)
         self.m_43 = nn.Identity()
-        self.m_44 = LayerNorm2d(384)
-        self.m_45 = nn.Conv2d(384, 384, kernel_size=1, stride=1, padding=0, bias=False)
-        self.m_46 = nn.GELU()
-        self.m_47 = nn.Dropout(p=0.5)
-        self.m_48 = nn.Conv2d(384, 384, kernel_size=1, stride=1, padding=0, bias=False)
-        self.m_49 = nn.Identity()
+        self.m_44 = nn.ReLU(inplace=True)
+        self.m_45 = nn.Conv2d(256, 512, kernel_size=3, stride=2, padding=1, groups=1, dilation=1, bias=False)
+        self.m_46 = nn.BatchNorm2d(512)
+        self.m_47 = nn.ReLU(inplace=True)
+        self.m_48 = nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1, groups=1, dilation=1, bias=False)
+        self.m_49 = nn.BatchNorm2d(512)
         self.m_50 = nn.Identity()
-        self.m_51 = LayerNorm2d(384)
-        self.m_52 = MHSA2D(384, heads=8, attn_drop=0, proj_drop=0)
-        self.m_53 = nn.Identity()
-        self.m_54 = LayerNorm2d(384)
-        self.m_55 = nn.Conv2d(384, 384, kernel_size=1, stride=1, padding=0, bias=False)
-        self.m_56 = nn.GELU()
-        self.m_57 = nn.Dropout(p=0.5)
-        self.m_58 = nn.Conv2d(384, 384, kernel_size=1, stride=1, padding=0, bias=False)
-        self.m_59 = nn.Identity()
-        self.m_60 = nn.Identity()
-        self.m_61 = nn.AdaptiveAvgPool2d(1)
-        self.m_62 = nn.Linear(384, 10)
+        self.m_proj_50 = nn.Sequential(
+            nn.Conv2d(256, 512, kernel_size=1, stride=2, padding=0, bias=False),
+            nn.BatchNorm2d(512)
+        )
+        self.m_51 = nn.ReLU(inplace=True)
+        self.m_52 = nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1, groups=1, dilation=1, bias=False)
+        self.m_53 = nn.BatchNorm2d(512)
+        self.m_54 = nn.ReLU(inplace=True)
+        self.m_55 = nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1, groups=1, dilation=1, bias=False)
+        self.m_56 = nn.BatchNorm2d(512)
+        self.m_57 = nn.Identity()
+        self.m_58 = nn.ReLU(inplace=True)
+        self.m_59 = nn.AdaptiveAvgPool2d(1)
+        self.m_60 = nn.Linear(512, 10)
 
     def forward(self, x):
         ys = []
@@ -118,10 +100,7 @@ class GeneratedModel(nn.Module):
         ys.append(x)
         x = self.m_2(x)
         ys.append(x)
-        res = ys[2]
-        if res.shape[2:] != x.shape[2:]:
-            res = F.adaptive_avg_pool2d(res, x.shape[2:])
-        x = x + res  # Residual add
+        x = self.m_3(x)
         ys.append(x)
         x = self.m_4(x)
         ys.append(x)
@@ -131,30 +110,27 @@ class GeneratedModel(nn.Module):
         ys.append(x)
         x = self.m_7(x)
         ys.append(x)
-        x = self.m_8(x)
-        ys.append(x)
-        res = ys[8]
+        res = ys[2]
         if res.shape[2:] != x.shape[2:]:
             res = F.adaptive_avg_pool2d(res, x.shape[2:])
         x = x + res  # Residual add
         ys.append(x)
-        res = ys[0]
-        if res.shape[2:] != x.shape[2:]:
-            res = F.adaptive_avg_pool2d(res, x.shape[2:])
-        x = x + res  # Residual add
+        x = self.m_9(x)
+        ys.append(x)
+        x = self.m_10(x)
         ys.append(x)
         x = self.m_11(x)
         ys.append(x)
         x = self.m_12(x)
         ys.append(x)
-        res = ys[12]
-        if res.shape[2:] != x.shape[2:]:
-            res = F.adaptive_avg_pool2d(res, x.shape[2:])
-        x = x + res  # Residual add
+        x = self.m_13(x)
         ys.append(x)
         x = self.m_14(x)
         ys.append(x)
-        x = self.m_15(x)
+        res = ys[9]
+        if res.shape[2:] != x.shape[2:]:
+            res = F.adaptive_avg_pool2d(res, x.shape[2:])
+        x = x + res  # Residual add
         ys.append(x)
         x = self.m_16(x)
         ys.append(x)
@@ -162,24 +138,17 @@ class GeneratedModel(nn.Module):
         ys.append(x)
         x = self.m_18(x)
         ys.append(x)
-        res = ys[18]
-        if res.shape[2:] != x.shape[2:]:
-            res = F.adaptive_avg_pool2d(res, x.shape[2:])
-        x = x + res  # Residual add
+        x = self.m_19(x)
         ys.append(x)
-        res = ys[10]
-        if res.shape[2:] != x.shape[2:]:
-            res = F.adaptive_avg_pool2d(res, x.shape[2:])
-        x = x + res  # Residual add
+        x = self.m_20(x)
         ys.append(x)
         x = self.m_21(x)
         ys.append(x)
-        x = self.m_22(x)
-        ys.append(x)
-        res = ys[22]
-        if res.shape[2:] != x.shape[2:]:
-            res = F.adaptive_avg_pool2d(res, x.shape[2:])
+        res = ys[16]
+        res = self.m_proj_22(res)
         x = x + res  # Residual add
+        ys.append(x)
+        x = self.m_23(x)
         ys.append(x)
         x = self.m_24(x)
         ys.append(x)
@@ -191,50 +160,40 @@ class GeneratedModel(nn.Module):
         ys.append(x)
         x = self.m_28(x)
         ys.append(x)
-        res = ys[28]
+        res = ys[23]
         if res.shape[2:] != x.shape[2:]:
             res = F.adaptive_avg_pool2d(res, x.shape[2:])
         x = x + res  # Residual add
         ys.append(x)
-        res = ys[20]
-        if res.shape[2:] != x.shape[2:]:
-            res = F.adaptive_avg_pool2d(res, x.shape[2:])
-        x = x + res  # Residual add
+        x = self.m_30(x)
         ys.append(x)
         x = self.m_31(x)
         ys.append(x)
         x = self.m_32(x)
         ys.append(x)
-        res = ys[32]
-        if res.shape[2:] != x.shape[2:]:
-            res = F.adaptive_avg_pool2d(res, x.shape[2:])
-        x = x + res  # Residual add
+        x = self.m_33(x)
         ys.append(x)
         x = self.m_34(x)
         ys.append(x)
         x = self.m_35(x)
         ys.append(x)
-        x = self.m_36(x)
+        res = ys[30]
+        res = self.m_proj_36(res)
+        x = x + res  # Residual add
         ys.append(x)
         x = self.m_37(x)
         ys.append(x)
         x = self.m_38(x)
         ys.append(x)
-        res = ys[38]
-        if res.shape[2:] != x.shape[2:]:
-            res = F.adaptive_avg_pool2d(res, x.shape[2:])
-        x = x + res  # Residual add
+        x = self.m_39(x)
         ys.append(x)
-        res = ys[30]
-        if res.shape[2:] != x.shape[2:]:
-            res = F.adaptive_avg_pool2d(res, x.shape[2:])
-        x = x + res  # Residual add
+        x = self.m_40(x)
         ys.append(x)
         x = self.m_41(x)
         ys.append(x)
         x = self.m_42(x)
         ys.append(x)
-        res = ys[42]
+        res = ys[37]
         if res.shape[2:] != x.shape[2:]:
             res = F.adaptive_avg_pool2d(res, x.shape[2:])
         x = x + res  # Residual add
@@ -249,24 +208,17 @@ class GeneratedModel(nn.Module):
         ys.append(x)
         x = self.m_48(x)
         ys.append(x)
-        res = ys[48]
-        if res.shape[2:] != x.shape[2:]:
-            res = F.adaptive_avg_pool2d(res, x.shape[2:])
-        x = x + res  # Residual add
+        x = self.m_49(x)
         ys.append(x)
-        res = ys[40]
-        if res.shape[2:] != x.shape[2:]:
-            res = F.adaptive_avg_pool2d(res, x.shape[2:])
+        res = ys[44]
+        res = self.m_proj_50(res)
         x = x + res  # Residual add
         ys.append(x)
         x = self.m_51(x)
         ys.append(x)
         x = self.m_52(x)
         ys.append(x)
-        res = ys[52]
-        if res.shape[2:] != x.shape[2:]:
-            res = F.adaptive_avg_pool2d(res, x.shape[2:])
-        x = x + res  # Residual add
+        x = self.m_53(x)
         ys.append(x)
         x = self.m_54(x)
         ys.append(x)
@@ -274,25 +226,18 @@ class GeneratedModel(nn.Module):
         ys.append(x)
         x = self.m_56(x)
         ys.append(x)
-        x = self.m_57(x)
+        res = ys[51]
+        if res.shape[2:] != x.shape[2:]:
+            res = F.adaptive_avg_pool2d(res, x.shape[2:])
+        x = x + res  # Residual add
         ys.append(x)
         x = self.m_58(x)
         ys.append(x)
-        res = ys[58]
-        if res.shape[2:] != x.shape[2:]:
-            res = F.adaptive_avg_pool2d(res, x.shape[2:])
-        x = x + res  # Residual add
-        ys.append(x)
-        res = ys[50]
-        if res.shape[2:] != x.shape[2:]:
-            res = F.adaptive_avg_pool2d(res, x.shape[2:])
-        x = x + res  # Residual add
-        ys.append(x)
-        x = self.m_61(x)
+        x = self.m_59(x)
         ys.append(x)
         if x.dim() > 2:
             x = F.adaptive_avg_pool2d(x, 1)
         x = torch.flatten(x, 1)
-        x = self.m_62(x)
+        x = self.m_60(x)
         ys.append(x)
         return x
